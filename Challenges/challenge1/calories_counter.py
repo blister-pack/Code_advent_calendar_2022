24c21f7d1d93bd1a944cd67898a3275daa8edff0
blister-pack <119133995+blister-pack@users.noreply.github.com>
2022-12-08 21:57:57 +0000

    solution for the second part of Day1 Code Advent Calendar

--- Challenges/challenge1/calories_counter.py
+++ Challenges/challenge1/calories_counter.py
@@ -2,15 +2,29 @@ file = open('calories.txt', 'r')
 read = file.readlines()
 
 calories_onElf = []
-totalCals_onElf = 0
+totalCals_onElf3 = 0
+totalCals_onElf2 = 0
+totalCals_onElf1 = 0
+
 
 for line in read:
     if line != '\n':
         calories_onElf.append(int(line[:-1]))  #  it works but appends with '\n' and that's going to ruin the sum, at the end -- solved
                                                #  I slice off the end of what I don't want and append it to the list as an int
     else:
-        if sum(calories_onElf) > totalCals_onElf:
-            totalCals_onElf = sum(calories_onElf)
+        if sum(calories_onElf) > totalCals_onElf1:
+            totalCals_onElf3 = totalCals_onElf2
+            totalCals_onElf2 = totalCals_onElf1
+            totalCals_onElf1 = sum(calories_onElf)
+
+        elif sum(calories_onElf) > totalCals_onElf2:
+            totalCals_onElf3 = totalCals_onElf2
+            totalCals_onElf2 = sum(calories_onElf)
+
+        elif sum(calories_onElf) > totalCals_onElf3:
+            totalCals_onElf3 = sum(calories_onElf)
         calories_onElf.clear()
 
-print(totalCals_onElf)
+
+
+print(totalCals_onElf1 + totalCals_onElf2 + totalCals_onElf3)
