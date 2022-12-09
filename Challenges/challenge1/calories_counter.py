6f03da6d3d8f0769f0ccba77846e4f24811d825f
blister-pack <119133995+blister-pack@users.noreply.github.com>
2022-12-09 18:19:08 +0000

    Day1 Code Advent Calendar Cleaned codei

--- Challenges/challenge1/calories_counter.py
+++ Challenges/challenge1/calories_counter.py
@@ -6,24 +6,14 @@ totalCals_onElf3 = 0
 totalCals_onElf2 = 0
 totalCals_onElf1 = 0
 
-
 for line in read:
     if line != '\n':
         calories_onElf.append(int(line[:-1]))  #  it works but appends with '\n' and that's going to ruin the sum, at the end -- solved
                                                #  I slice off the end of what I don't want and append it to the list as an int
     else:
-        if sum(calories_onElf) > totalCals_onElf1:
-            totalCals_onElf3 = totalCals_onElf2
-            totalCals_onElf2 = totalCals_onElf1
-            totalCals_onElf1 = sum(calories_onElf)
-
-        elif sum(calories_onElf) > totalCals_onElf2:
-            totalCals_onElf3 = totalCals_onElf2
-            totalCals_onElf2 = sum(calories_onElf)
-
-        elif sum(calories_onElf) > totalCals_onElf3:
+        if sum(calories_onElf) > totalCals_onElf3:
             totalCals_onElf3 = sum(calories_onElf)
-        calories_onElf.clear()
+            top3_elves = [totalCals_onElf1, totalCals_onElf2, totalCals_onElf3]
 
 print(totalCals_onElf1 + totalCals_onElf2 + totalCals_onElf3)
 
