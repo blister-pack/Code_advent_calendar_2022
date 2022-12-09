ffb4e23b3979e74f4836cc1fdd1c55d441f80020
blister-pack <119133995+blister-pack@users.noreply.github.com>
2022-12-09 20:29:44 +0000

    Day1 Code Advent Calendar Cleaned code

---
+++ Challenges/challenge3/ruck$ack$orter.py
@@ -0,0 +1,13 @@
+file = open('letters.txt', 'r')
+read = file.readlines()
+
+#  dictionary with letters of the alphabet and their positions
+
+for rucksack in read:
+    no_items = len(rucksack) - 1
+
+    pocket1 = rucksack[:(no_items//2)]
+    pocket2 = rucksack[no_items//2:no_items]
+
+    #  only works with //, with / Python returns a float and that creates an error
+
