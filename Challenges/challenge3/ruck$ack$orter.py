2d62db0159def0bc6d4fb025470ef53e7ba18641
blister-pack <119133995+blister-pack@users.noreply.github.com>
2022-12-10 21:33:05 +0000

    Day3 Part1
    Program works if I add another line to the .txt file

--- Challenges/challenge3/ruck$ack$orter.py
+++ Challenges/challenge3/ruck$ack$orter.py
@@ -8,9 +8,12 @@ read = file.readlines()
 alphabet_lowerAndUpper = string.ascii_lowercase + string.ascii_uppercase
 
 points_and_letters = {}
+
+
 for number, letter in enumerate(alphabet_lowerAndUpper):
     points_and_letters[letter] = number + 1
     #  created the dict with the alphabet and their respective point values
+value_of_items = 0
 
 for rucksack in read:
     no_items = len(rucksack) - 1
@@ -19,11 +22,9 @@ for rucksack in read:
     pocket2 = rucksack[no_items//2:no_items]
     #  only works with //, with / Python returns a float and that creates an error
 
-    value_of_items = 0
-
     for letter in pocket1:
-        for letter2 in pocket2:
-            if letter == letter2:
-                value_of_items = value_of_items + points_and_letters[letter]
+        if letter in pocket2:
+            value_of_items = value_of_items + points_and_letters[letter]
+            break
 
 print(value_of_items)
