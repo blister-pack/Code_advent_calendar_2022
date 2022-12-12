b634cf05c9e5cf74a6c0c165c6eab5f931e17a53
blister-pack <119133995+blister-pack@users.noreply.github.com>
2022-12-12 17:15:56 +0100

    Update cl3aning_s3ctions.py

---
+++ Challenges/challenge4/cl3aning_s3ctions.py
@@ -0,0 +1,8 @@
+with open('input4.txt','r') as f:
+    f_contents = f.readlines()
+
+    for section_pairs in f_contents:
+        areas = section_pairs.partition(',')
+        #  how do I save this without the '\n'
+        elf1_area = str(areas[0])
+        elf2_area[:-1] = str(areas[2])
