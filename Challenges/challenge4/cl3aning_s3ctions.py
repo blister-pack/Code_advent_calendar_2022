1ff41d0426b520d48d6d512ed73b40d353795659
blister-pack <119133995+blister-pack@users.noreply.github.com>
2022-12-14 19:41:09 +0100

    Update cl3aning_s3ctions.py

--- Challenges/challenge4/cl3aning_s3ctions.py
+++ Challenges/challenge4/cl3aning_s3ctions.py
@@ -1,9 +1,17 @@
 with open('input4.txt','r') as f:
     f_contents = f.readlines()
-
+    overlap_areas = 0
     for section_pairs in f_contents:
         areas = section_pairs.partition(',')
         #  how do I save this without the '\n'
-        elf1_area = str(areas[0])
-        elf2_area = str(areas[2])
-        
+        elf1_area = areas[0].split('-')
+        elf2_area = areas[2][:-1].split('-')
+
+
+
+        first_area = range(int(elf1_area[0]), int(elf1_area[1]))
+        second_area = range(int(elf2_area[0]), int(elf2_area[1]))
+
+        if first_area in second_area or second_area in first_area:
+            overlap_areas += 1
+            #  it's not counting
