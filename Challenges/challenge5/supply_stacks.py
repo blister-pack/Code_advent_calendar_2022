77d3aa24966bf7a414094abcb7bbc61b2e197d4c
blister-pack <119133995+blister-pack@users.noreply.github.com>
2022-12-16 08:51:46 +0100

    Day5 part1

--- Challenges/challenge5/supply_stacks.py
+++ Challenges/challenge5/supply_stacks.py
@@ -1,4 +1,24 @@
 stack1 = ['S', 'Z', 'P', 'D', 'L', 'B', 'F', 'C']
 stack2 = ['N', 'V', 'G', 'P', 'H', 'W', 'B']
 stack3 = ['F', 'W', 'B', 'J', 'G']
-stack4 = ['G', 'J', 'N', '']
+stack4 = ['G', 'J', 'N', 'F', 'L', 'W', 'C', 'S']
+stack5 = ['W', 'J', 'L', 'T', 'P', 'M', 'S', 'H']
+stack6 = ['B', 'C', 'W', 'G', 'F', 'S']
+stack7 = ['H', 'T', 'P', 'M', 'Q', 'B', 'W']
+stack8 = ['F', 'S', 'W', 'T']
+stack9 = ['N', 'C', 'R']
+
+with open('input5.txt', 'r') as f:
+    f_content = f.readlines()[10:]
+
+    for move_order in f_content:
+        supp = move_order.split()
+        boxes_to_move = int(supp[1])
+        from_stack = globals()[f'stack{supp[3]}']
+        to_stack = globals()[f'stack{supp[5]}']
+
+        for numBoxes in range(0, boxes_to_move):
+            movedBox = from_stack.pop()
+            to_stack.append(movedBox)
+
+print(f'{stack1[-1]}{stack2[-1]}{stack3[-1]}{stack4[-1]}{stack5[-1]}{stack6[-1]}{stack7[-1]}{stack8[-1]}{stack9[-1]}')
